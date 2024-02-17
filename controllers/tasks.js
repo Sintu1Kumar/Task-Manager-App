ab316281a8932d5a4fa8a7a2607b27cc04cb9b7a
Sintu Kumar <kumarsintu499@gmail.com>
2024-02-17 17:33:56 +0530

    first commit

---
+++ controllers/tasks.js
@@ -0,0 +1,24 @@
+const Task = require("../models/Task");
+
+const getAllTasks = (req, res) => {
+  res.send("all tasks");
+};
+
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).send(task);
+};
+
+const getTask = (req, res) => {
+  res.send("get single task");
+};
+
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
