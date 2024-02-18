aeab2f836f9530e809ab6f44cad578e4a9ea4098
Sintu Kumar <kumarsintu499@gmail.com>
2024-02-18 14:12:18 +0530

    connect with DB

--- models/Task.js
+++ models/Task.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: [true, "Please Enter Task Name"],
+    trim: true,
+    minlength: [2, "Task name can not be less then 2 characters"],
+    maxlength: [20, "Task name can not be greater then 20 characters"],
+  },
   completed: Boolean,
 });
 
